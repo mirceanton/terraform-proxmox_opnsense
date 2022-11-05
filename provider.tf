4b94f553af27ebb9634f2f1f2ba9588f70d7ec2f
Mircea-Pavel Anton <contact@mirceanton.com>
2022-11-05 22:17:05 +0200

    [Release] v1.0.0 (#1)
    * Ignore the terraform lock file
    
    * Update the changelog
    
    * Implement template.
    
    * Update README
    
    * Add pipeline
    
    * Add PR template

---
+++ provider.tf
@@ -0,0 +1,34 @@
+# ===============================================
+# Terraform configuration
+# ===============================================
+terraform {
+  required_providers {
+    proxmox = {
+      source  = "telmate/proxmox"
+      version = "2.9.10"
+    }
+  }
+}
+
+# ===============================================
+# Proxmox Provider Configuration
+# ===============================================
+provider "proxmox" {
+  pm_api_url          = var.proxmox_api_url
+  pm_api_token_id     = var.proxmox_api_token_id
+  pm_api_token_secret = var.proxmox_api_token_secret
+}
+
+variable "proxmox_api_url" {
+  type = string
+}
+
+variable "proxmox_api_token_id" {
+  type      = string
+  sensitive = true
+}
+
+variable "proxmox_api_token_secret" {
+  type      = string
+  sensitive = true
+}
